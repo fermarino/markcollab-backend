683a25f8290b37acdebe33f6d568a15886d6d7d1
Clara-oliveiraa <claraoliveira.8351@gmail.com>
2024-12-03 00:35:44 -0300

    fix: web config

--- src/main/java/com/markcollab/config/WebConfig.java
+++ src/main/java/com/markcollab/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.markcollab.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permitir todas as rotas
-                .allowedOrigins("http://localhost:5173") // Adicionar o frontend aqui
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**") // Permite todas as rotas
+                .allowedOrigins("*") // Permite todas as origens
+                .allowedMethods("*") // Permite todos os métodos (GET, POST, PUT, DELETE, etc.)
+                .allowedHeaders("*") // Permite todos os cabeçalhos
+                .allowCredentials(false); // Desabilita o uso de cookies/autenticação via credenciais
     }
 }
