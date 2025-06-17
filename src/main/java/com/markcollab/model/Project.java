0de54b2dff0b6782808537f620a11bcb0f895abe
fermarino <fnandomarino@gmail.com>
2025-06-17 01:36:39 -0300

    fix project

--- src/main/java/com/markcollab/model/Project.java
+++ src/main/java/com/markcollab/model/Project.java
@@ -1,54 +1,43 @@
 package com.markcollab.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import java.time.LocalDate;
-import java.util.Set;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+import java.time.LocalDate;
+import java.util.List;
+
 @Entity
 @Table(name = "projects")
+@Data
 public class Project {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long projectId;
 
-    @Column(nullable = false, length = 150)
     private String projectTitle;
-
-    @Column(columnDefinition = "TEXT", nullable = false)
     private String projectDescription;
-
-    @Column(columnDefinition = "TEXT", nullable = false)
     private String projectSpecifications;
-
-    @Column(nullable = false)
     private Double projectPrice;
-
-    @Column(nullable = false)
-    private LocalDate deadline;
-
-    @Column(nullable = false)
     private boolean open;
-
-    @Column(nullable = false)
     private String status;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "employer_cpf", nullable = false)
+    private LocalDate deadline;
+
+    @ManyToOne
+    @JoinColumn(name = "employer_cpf")
     private Employer projectEmployer;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "hired_freelancer_cpf")
     private Freelancer hiredFreelancer;
 
+    /**
+     * Observação: a fetch type padrão para OneToMany é LAZY.
+     * Graças ao @Transactional no service, não haverá LazyInitializationException.
+     */
     @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Interest> interestedFreelancers;
+    @JsonIgnore
+    private List<Interest> interestedFreelancers;
 }
